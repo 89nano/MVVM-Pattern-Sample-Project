f087b286828665f5c55c304685e739ffdcf2389d
Christian Munoz <cmunoz@nd-soft.com>
2019-01-08 11:14:50 -0400

    Restore files after accidental reset of branch

--- MVVM Pattern Sample Project/ViewModels/DemographicsViewModel.cs
+++ MVVM Pattern Sample Project/ViewModels/DemographicsViewModel.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MVVM_Pattern_Sample_Project.Model;
 
 namespace MVVM_Pattern_Sample_Project.ViewModels
 {
-    public class PatientViewModel
+    public class DemographicsViewModel
     {
+
+        
     }
 }
