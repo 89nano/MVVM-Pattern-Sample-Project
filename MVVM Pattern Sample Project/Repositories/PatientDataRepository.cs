1c4b0decde08b62f2029b00bb56e7ddf035db318
Christian Munoz <cmunoz@nd-soft.com>
2019-01-14 11:05:05 -0400

    Added Respository and Collections Manager Service

---
+++ MVVM Pattern Sample Project/Repositories/PatientDataRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVM_Pattern_Sample_Project.Model;
+using MVVM_Pattern_Sample_Project.Properties;
+using Newtonsoft.Json;
+
+namespace MVVM_Pattern_Sample_Project.Repositories
+{
+    public class PatientDataRepository
+    {
+
+        public void WriteToJsonFile(PatientModel Model)
+        {
+
+            var output = JsonConvert.SerializeObject(Model);
+            output = output.Replace("[{", "{");
+            output = output.Replace("}]", "}");
+
+            File.WriteAllText(Resources.ApplicationDataFileDirectory, output);
+
+        }
+
+
+        public string ReadJsonFile()
+        {
+            using (StreamReader reader = new StreamReader(Resources.ApplicationDataFileDirectory))
+            {
+                string json;
+
+                try
+                {
+                    json = reader.ReadToEnd();
+                }
+                catch (FileNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                    throw;
+                }
+
+                return json;
+            }
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ MVVM Pattern Sample Project/Services/CollectionsManagerService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM_Pattern_Sample_Project.Services
+{
+    public class CollectionsManagerService
+    {
+
+        public ObservableCollection<string> AddDelimitedValuesToCollection(string delimitedValues, char delimiter,ObservableCollection<string> Collection)
+        {
+
+            var splittedCollection = delimitedValues.Split(delimiter);
+
+            foreach (var item in splittedCollection)
+            {
+                if (!Collection.Contains(item) && !string.IsNullOrEmpty(item))
+                {
+                    Collection.Add(item);
+                }
+                else
+                {
+                    MessageBox.Show("Item is already in the list");
+                }
+
+            }
+
+            return Collection;
+        }
+
+
+        public ObservableCollection<string> RemoveFromCollection(int workingListBoxIndex,
+            ObservableCollection<string> Collection)
+        {
+
+            Collection.RemoveAt(workingListBoxIndex);
+
+            return Collection;
+        }
+
+    }
+}
